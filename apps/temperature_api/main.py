06537e32a84380c4118e8bf9b96344a80880ffeb
ayakhont <ayakhont@gmail.com>
2025-09-18 02:49:25 +0300

    5th task

--- apps/temperature_api/main.py
+++ apps/temperature_api/main.py
@@ -1,32 +1,48 @@
-from fastapi import FastAPI, HTTPException
+import random
+from typing import Optional
+
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field
 import uvicorn
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
 
 
-app.get("/temperature/{location}",
-        response_model=float,
-        tags=["Temperature"],
-        summary="Get temperature for a location",
-        responses={200: {"description": "Temperature retrieved successfully"},
-                   404: {"description": "Location not found"},
-                   500: {"description": "Internal server error"}})
-def get_temperature(location: str):
-    if location == "":
+app.get("/health", tags=["Health"], summary="Health check endpoint")
+def health_check():
+    return {"status": "ok"}
+
+
+@app.get("/temperature")
+@app.get("/temperature/{sensor_id}")
+def get_temperature(
+    sensor_id: Optional[str] = None,
+    location: str = Query(default="", description="Название комнаты"),
+) :
+    if sensor_id:
+        match sensor_id:
+            case "1":
+                location = "Living Room"
+            case "2":
+                location = "Bedroom"
+            case "3":
+                location = "Kitchen"
+            case _:
+                location = "Unknown"
+    else:
         match location:
             case "Living Room":
-                temperature = 21.5
-            case "bedroom":
-                temperature = 20.0
-            case "kitchen":
-                temperature = 23.0
+                sensor_id = "1"
+            case "Bedroom":
+                sensor_id = "2"
+            case "Kitchen":
+                sensor_id = "3"
             case _:
-                raise HTTPException(status_code=404, detail="Location not found")
-    temperature = 22.5  # Dummy temperature value
-    return temperature
+                sensor_id = "0"
+    temperature = round(random.uniform(15.0, 30.0), 2)
+
+    return {"value":temperature, "sensorId": sensor_id, "location": location}
 
 def main():
     uvicorn.run("main:app", host="localhost", port=8000, reload=True)
